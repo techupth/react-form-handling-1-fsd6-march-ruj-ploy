7bf600e96191c792bda9e9242e5aecc448abf636
AtirujRat <136755600+RattRuj@users.noreply.github.com>
2024-06-05 13:09:10 +0700

    feat:add func for input and add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+  const [greetingMessageInput, setGreeingMessageInput] = useState("");
+
+  const greetingMessageInputHandle = (e) => {
+    setGreeingMessageInput(e.target.value);
+  };
+
+  const sunmitHandle = (e) => {
+    e.preventDefault();
+    if (greetingMessage !== "") {
+      setGreetingMessage(greetingMessageInput);
+    } else {
+      return;
+    }
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
-      <div className="input-container">
-        <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
-      </div>
+      <div className="greeting-container">{greetingMessage}</div>
+      <form onSubmit={sunmitHandle} action="">
+        <div className="input-container">
+          <label htmlFor="greeting-message">New Greeting Message</label>
+          <input
+            onChange={greetingMessageInputHandle}
+            value={greetingMessageInput}
+            id="greeting-message"
+            type="text"
+          />
+        </div>
 
-      <div className="buttons">
-        <button>Update text</button>
-      </div>
+        <div className="buttons">
+          <button>Update text</button>
+        </div>
+      </form>
     </div>
   );
 }
