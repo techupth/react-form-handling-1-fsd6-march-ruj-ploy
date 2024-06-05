ce932a6c154e9629ac0c6d87670bbc05434131e6
nattawara <nattawara.jtk@gmail.com>
2024-06-05 13:54:38 +0700

    feat: add onChange, onClick event

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessageInput, setGreetingMessageInput] = useState("");
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+  const handleChange = (event) => {
+    setGreetingMessageInput(event.target.value);
+  };
+  const handleClick = () => {
+    setGreetingMessage(greetingMessageInput);
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={greetingMessageInput}
+          onChange={handleChange}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleClick}>Update text</button>
       </div>
     </div>
   );
